96f827b4ff49a6b3400d16088079f852a9bb1360
Nikola Jovanovski <nikola_ate@hotmail.com>
2021-12-04 20:28:55 +0100

    Created parser.py file for the parsing program.

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 # imports go here
+from  parser import Parser
+
+
 
 def main():
    """Main entry"""
-   pass
+   with open("example.txt", "r") as file:
+      p1 = Parser(file)
+      
+      sentences, snt_count, char_count = p1.parsing()
+
+      print(sentences, "\n\n")
+
+      print(f'Found total of {snt_count} sentences. With total of {char_count} character in the file {file.name}')
+
 
 if __name__ == '__main__':
    main()
